0f6ad17e1e7288f18c493a6cf139a690c221e8b3
Сергей Орлов <orlov.2000@mail.ru>
2024-02-23 16:01:05 +0300

    4commit prog1 19/02

--- 20240219/1/prog.py
+++ 20240219/1/prog.py
@@ -41,3 +41,48 @@ else:
             print(f"blob {num.hex()}\t{tname.decode()}")
         else:
             print(f"tree {num.hex()}\t{tname.decode()}")
+    print()
+    while body.decode().split('\n', 2)[1].split()[0] == "parent":
+        print(f"TREE for commit {commit_sha}")
+
+        tree_sha = body.decode().split('\n', 1)[0].split()[1]
+
+        with open(os.path.join(objects_dir, tree_sha[:2], tree_sha[2:]), "rb") as fd:
+            tree_obj, _, tail = zlib.decompress(fd.read()).partition(b'\x00')
+
+        while tail:
+            treeobj, _, tail = tail.partition(b'\x00')
+            tmode, tname = treeobj.split()
+            num, tail = tail[:20], tail[20:]
+            if tmode[:3] == b'100':
+                print(f"blob {num.hex()}\t{tname.decode()}")
+            else:
+                print(f"tree {num.hex()}\t{tname.decode()}")
+
+        commit_sha = body.decode().split('\n', 2)[1].split()[1]
+        objects_path = os.path.join(objects_dir, commit_sha[:2], commit_sha[2:])
+
+        flag = True
+        i = 2
+
+        while not os.path.exists(objects_path):
+            flag = False
+            if body.decode().split('\n', i+1)[i].split()[0] != "parent":
+                break
+            while not flag:
+                if body.decode().split('\n', i+1)[i].split()[0] == "parent":
+                    commit_sha = body.decode().split('\n', i+1)[i].split()[1]
+                    objects_path = os.path.join(objects_dir, commit_sha[:2], commit_sha[2:])
+                    flag = True
+                else:
+                    break
+
+                i += 1
+
+        if flag:
+            with open(objects_path, "rb") as fd:
+                obj = zlib.decompress(fd.read())
+
+            header, _, body = obj.partition(b'\x00')
+        else:
+            break
