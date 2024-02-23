a2d4e6d420d94ec5c84efbd8f305f3b94c67833b
Сергей Орлов <orlov.2000@mail.ru>
2024-02-23 14:57:51 +0300

    2commit prog1 19/02

--- 20240219/1/prog.py
+++ 20240219/1/prog.py
@@ -10,4 +10,21 @@ refs_heads_dir = os.path.join(repo_path, '.git', 'refs', 'heads')
 if len(arg) == 1:
     print(*os.listdir(refs_heads_dir), sep='\n')
 else:
-    pass
+    br_name = arg[1]
+    branch_file_path = os.path.join(refs_heads_dir, br_name)
+
+    if not os.path.exists(branch_file_path):
+        sys.exit()
+
+    with open(branch_file_path, "r") as fd:
+        commit_sha = fd.read().strip()
+
+    objects_dir = os.path.join(repo_path, '.git', 'objects')
+    objects_path = os.path.join(objects_dir, commit_sha[:2], commit_sha[2:])
+
+    with open(objects_path, "rb") as fd:
+        obj = zlib.decompress(fd.read())
+
+    header, _, body = obj.partition(b'\x00')
+
+    print(body.decode())
